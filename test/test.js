bf101fde5f50792303e59e0cd7e190d2fbf3e287
David Gagne <1399002+thegagne@users.noreply.github.com>
2020-08-23 16:20:56 -0500

    adding delay feature.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import { ControlledRequest, controlledFetch } from "../trafficControl/trafficControl.js";
 import {
+  assert,
   assertEquals,
   assertStringContains,
 } from "https://deno.land/std/testing/asserts.ts";
@@ -22,4 +23,19 @@ Deno.test("retry - 500", async () => {
   const trafficInfoHeader = res.headers.get("traffic-info");
   await res.text();
   assertStringContains(trafficInfoHeader, "retries=3");
+});
+
+
+Deno.test("delay - 300ms", async () => {
+  const trafficPolicy = {
+    delay: 300,
+  };
+  const req = new ControlledRequest("https://success-200.thegagne.workers.dev", {
+    trafficPolicy: trafficPolicy,
+  });
+  const before = Date.now();
+  const res = await controlledFetch(req);
+  const after = Date.now();
+  await res.text();
+  assert(after - 300 >= before);
 });
